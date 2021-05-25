8d640d2af7a6ff94113ea8c726e33b6376aa167e
ravih18 <rhassana96@gmail.com>
2021-05-25 11:22:15 +0200

    Added GradCam attention map tools for vae

---
+++ clinicadl/clinicadl/visualize/gradcam/gradcam_cli.py
@@ -0,0 +1,95 @@
+"""
+clinicadl gradcam model_path caps_dir dataset_type tsv_path -cpu --nproc --verbose
+"""
+
+def gradcam_func(args):
+    from .gradcam_vae import attention_map
+    attention_map(args)
+
+def create_gradcam_parser(subparser):
+
+    from clinicadl.utils.cli_utils import parent_parser
+
+    gradcam_parser = subparser.add_parser(
+        'gradcam',
+        parents=[parent_parser],
+        help='''Create gradcams on testset for defined model.'''
+    )
+
+    gradcam_parser.add_argument(
+        "output_dir", type=str,
+        help="Path to the trained model folder."
+    )
+
+    gradcam_parser.add_argument(
+        'caps_dir',
+        help='Data using CAPS structure.',
+        default=None
+    )
+
+    gradcam_parser.add_argument(
+        'mode',
+        help='''Create gradcams on testset for defined model.''',
+        choices=['image', 'slice', 'patch', 'roi'],
+        type=str
+    )
+
+    gradcam_parser.add_argument(
+        'preprocessing',
+        help='Defines the type of preprocessing of CAPS data.',
+        choices=['t1-linear', 't1-extensive', 't1-volume', 'shepplogan'],
+        type=str
+    )
+
+    gradcam_parser.add_argument(
+        'tsv_path',
+        help='TSV path with subjects/sessions to process.',
+        default=None
+    )
+
+    gradcam_parser.add_argument(
+        '-cpu', '--use_cpu', action='store_true',
+        help='If provided, will use CPU instead of GPU.',
+        default=False
+    )
+
+    gradcam_parser.add_argument(
+        '-np', '--nproc',
+        help='Number of cores used during the training.',
+        type=int, default=2
+    )
+
+    gradcam_parser.add_argument(
+        '--batch_size',
+        default=2, type=int,
+        help='Batch size for training.'
+    )
+
+    gradcam_parser.add_argument(
+        '--unnormalize', '-un',
+        help='Disable default MinMaxNormalization.',
+        action="store_true",
+        default=False
+    )
+    
+    gradcam_parser.add_argument(
+        '--diagnoses', '-d',
+        help='List of diagnoses that will be selected for training.',
+        default=['AD', 'CN'], nargs='+', type=str,
+        choices=['AD', 'BV', 'CN', 'MCI', 'sMCI', 'pMCI']
+    )
+
+    gradcam_parser.add_argument(
+        "--baseline", action="store_true", default=False,
+        help="If provided, only the baseline sessions are used for training."
+    )
+
+    gradcam_parser.add_argument(
+        "--multi_cohort",
+        help="Performs multi-cohort training. In this case, caps_dir and tsv_path must be paths to TSV files.",
+        action="store_true",
+        default=False
+    )
+
+
+    gradcam_parser.set_defaults(func=gradcam_func)
---
+++ clinicadl/clinicadl/visualize/gradcam/gradcam_vae.py
@@ -0,0 +1,101 @@
+# coding: utf8
+
+import numpy as np
+import torch
+import os
+from torch.utils.data import DataLoader
+from PIL import Image
+
+from clinicadl.visualize.gradcam.gradcam_utils import GradCAM, save_cam
+from clinicadl.tools.deep_learning.models import init_model, load_model
+from clinicadl.tools.deep_learning.data import (load_data_test,
+                                                get_transforms,
+                                                return_dataset,
+                                                generate_sampler)
+from clinicadl.tools.deep_learning.iotools import return_logger, check_and_clean
+from clinicadl.tools.deep_learning.iotools import commandline_to_json, write_requirements_version, translate_parameters
+
+
+def attention_map(params):
+    """
+    Create attention map for a model (VAE) on a testing set to visualize
+    anomalies
+    """
+
+    main_logger = return_logger(params.verbose, "main process")
+    AMap_logger = return_logger(params.verbose, "attention map")
+    # check_and_clean(params.output_dir)
+
+    commandline_to_json(params, logger=main_logger)
+    # write_requirements_version(params.output_dir)
+    params = translate_parameters(params)
+
+    _, all_transforms = get_transforms(
+        params.mode,
+        minmaxnormalization=params.minmaxnormalization,
+    )
+
+    # Load dataset
+    test_df = load_data_test(
+        params.tsv_path,
+        params.diagnoses,
+        baseline=params.baseline
+    )
+
+    data_test = return_dataset(params.mode, params.input_dir, test_df, params.preprocessing,
+                               all_transformations=all_transforms, prepare_dl=params.prepare_dl,
+                               multi_cohort=params.multi_cohort, params=params)
+
+    test_loader = DataLoader(
+        data_test,
+        batch_size=params.batch_size,
+        shuffle=False,
+        num_workers=params.num_workers,
+        pin_memory=True
+    )
+
+    # Load model
+    model_dir = os.path.join(params.output_dir, 'fold-%i' % 0, 'models')
+    vae = init_model(params, initial_shape=data_test.size, architecture="vae")
+    model, _ = load_model(vae, os.path.join(model_dir, "best_loss"),
+                          params.gpu, filename='model_best.pth.tar')
+    # create GradCAM
+    mu_avg, logvar_avg = 0, 1
+    gcam = GradCAM(model, target_layer='encoder.2')
+
+    # create output dir
+    im_path = os.path.join(params.output_dir, 'attention_maps')
+    if not os.path.exists(im_path):
+        os.mkdir(im_path)
+
+    # loop on data set
+    for _, data in enumerate(test_loader):
+        model.eval()
+        if params.gpu:
+            imgs = data['image'].cuda()
+        else:
+            imgs = data['image']
+        _, mu, logvar = gcam.forward(imgs)
+
+        model.zero_grad()
+        gcam.backward(mu, logvar, mu_avg, logvar_avg)
+        gcam_map = gcam.generate()
+
+        ## Visualize and save attention maps  ##
+        # make a 3 channel image for colors
+        imgs = imgs.repeat(1, 3, 1, 1)
+        for i in range(imgs.size(0)):
+            # multiply by 255 to change from grey scale to colors
+            raw_image = imgs[i] * 255.0
+            ndarr = raw_image.permute(1, 2, 0).cpu().byte().numpy()
+            im = Image.fromarray(ndarr.astype(np.uint8))
+            # save original image
+            im.save(os.path.join(im_path,
+                                 "{}_{}-origin.png".format(data['participant_id'][0], data['session_id'][0])))
+
+            file_path = os.path.join(im_path,
+                                    "{}_{}-attmap.png".format(data['participant_id'][0], data['session_id'][0]))
+            print(file_path)
+            r_im = np.asarray(im)
+            # save grad cam
+            save_cam(r_im, file_path, gcam_map[i].squeeze().cpu().data.numpy())
