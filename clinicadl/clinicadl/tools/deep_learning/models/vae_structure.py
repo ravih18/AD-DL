20a82b79d2043abc05a84475ef50f553e809bba8
ravih18 <rhassana96@gmail.com>
2021-06-07 10:49:07 +0200

    Corrected feature size for 2D latent space

--- clinicadl/clinicadl/tools/deep_learning/models/vae_structure.py
+++ clinicadl/clinicadl/tools/deep_learning/models/vae_structure.py
@@ -98,7 +98,7 @@ class VAE_Encoder(nn.Module):
         elif latent_dim==2:
             self.layers.append(nn.Sequential(
                 nn.Conv2d(
-                    first_layer_channels * 2**(n_conv+1),
+                    first_layer_channels * 2**(n_conv - 1),
                     feature_size,
                     4, stride=1, padding=0, bias=False),
                 nn.ReLU()
